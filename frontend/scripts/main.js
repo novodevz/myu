0dff2c46d71424bcbc124839cbc738dad7c7e6b3
novodevz <78587100+novodevz@users.noreply.github.com>
2024-01-05 10:37:02 +0200

    implement image handler

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -15,17 +15,4 @@ async function loadPage(page) {
     }
 }
 
-
-// Function to fetch the welcome message
-function fetchWelcomeMessage() {
-    axios.get('http://localhost:5000/')  // Adjust the URL if needed
-        .then(response => {
-            // Update the welcome message in the HTML
-            document.getElementById('welcome-message').textContent = response.data.message;
-        })
-        .catch(error => {
-            console.error('Error fetching welcome message:', error);
-        });
-}
-
 // You can add more general functions here, if needed.
